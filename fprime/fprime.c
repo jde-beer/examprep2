d2fb8f70d3d33dd57167516a97fce0d2337d1ca4
jde-beer <50702577+jde-beer@users.noreply.github.com>
2019-12-17 12:58:08 +0200

    Add files via upload

--- fprime/fprime.c
+++ fprime/fprime.c
@@ -4,24 +4,22 @@
 void	fprime(int n)
 {
 	int i = 2;
-	if (n == 1)
-		printf("1");
+	if(n == 1)
+		printf("1");;
 	while(n > 1)
 	{
-		if (n % i == 0)
+		if(n % i == 0)
 		{
 			n /= i;
-			if (i != 1)
+			if (i > 1)
 				printf("%i", i);
-			if (n > 1)
+			if(n > 1)
 				printf("*");
-			
 		}
 		else
 			i++;
 	}
 }
-
 int		main(int argc, char **argv)
 {
 	if (argc == 2)
